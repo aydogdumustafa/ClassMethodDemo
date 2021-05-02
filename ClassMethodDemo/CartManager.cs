c3b3a372be6d39dab99bb21ec808aa34171ee7cd
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-05-02 16:32:08 +0200

    Add project files.

---
+++ ClassMethodDemo/CartManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CartManager
+    {
+        public static void AddToCart(Product product)
+        {
+            Console.WriteLine("{0} is added to cart for {1} $", product.Name, product.Price);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Chocolate";
+            product1.Price = 3.12;
+
+            Product product2 = new Product();
+            product2.Name = "Milk";
+            product2.Price = 1.34;
+
+            Product product3 = new Product();
+            product3.Name = "Bread";
+            product3.Price = 0.46;
+
+            CartManager.AddToCart(product1);
+            CartManager.AddToCart(product2);
+            CartManager.AddToCart(product3);
+        }
+    }
+}
